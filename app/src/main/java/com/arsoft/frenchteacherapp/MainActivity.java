bad2226694a47af2c6d54b822e823958726f108e
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-15 21:29:11 +0600

    Final version

--- app/src/main/java/com/arsoft/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/arsoft/frenchteacherapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.arsoft.frenchteacherapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -9,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button redBtn, blackBtn, greenBtn, yellowBtn, purpleBtn;
 
@@ -18,9 +20,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        redBtn = findViewById(R.id.redBtn);
+        blackBtn = findViewById(R.id.blackBtn);
+        greenBtn = findViewById(R.id.greenBtn);
+        yellowBtn = findViewById(R.id.yellowBtn);
+        purpleBtn = findViewById(R.id.purpleBtn);
+
+        redBtn.setOnClickListener(this);
+        blackBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        yellowBtn.setOnClickListener(this);
+        purpleBtn.setOnClickListener(this);
 
 
 
 
     }
+
+    @Override
+    public void onClick(View v) {
+        // Find the button by ID and play the correct sound
+
+        int clickedBtnId = v.getId();
+
+        if (clickedBtnId == R.id.redBtn){
+            playSound(R.raw.red);
+        } else if(clickedBtnId == R.id.blackBtn){
+            playSound(R.raw.black);
+        } else if (clickedBtnId == R.id.greenBtn) {
+            playSound(R.raw.green);
+        } else if (clickedBtnId == R.id.purpleBtn) {
+            playSound(R.raw.purple);
+        } else if (clickedBtnId == R.id.yellowBtn) {
+            playSound(R.raw.yellow);
+        }
+
+    }
+
+    public void playSound(int id){
+        MediaPlayer mediaPlayer = MediaPlayer.create(
+                this,
+                id
+        );
+        mediaPlayer.start();
+    }
+
 }
