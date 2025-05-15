0e39df5e86d534c9812dee6d1d5d3a3aa66e5273
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-15 19:58:42 +0600

    updated ui

--- app/src/main/java/com/arsoft/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/arsoft/frenchteacherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arsoft.frenchteacherapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button redBtn, blackBtn, greenBtn, yellowBtn, purpleBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
 
     }
